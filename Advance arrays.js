5c81db4d0ed7e530543a69e2d789fd00a5f9447b
JohnPauloPaluan <jppaluan0808@gmail.com>
2024-05-04 16:46:09 -0400

    PracticeBonus

--- Advance arrays.js
+++ Advance arrays.js
@@ -29,29 +29,44 @@ const array = [
 
 //Create an array using forEach that has all the usernames with a "!" to each of the usernames
 
-const forEachArrays = array.forEach(obj => obj.username + "!");
-  
+const AdvanceArrays = []
+const forEachArrays = array.forEach((obj)=> 
+  { 
+  AdvanceArrays.push(obj.username + "!")
+  });
 
+console.log(AdvanceArrays);
+  
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
 
 const mapArrays = array.map(obj => obj.username + "?");
+console.log(mapArrays);
 
 
 //Filter the array to only include users who are on team: red
 
 const filterArrays = array.filter(obj => {return obj.team === "red" });
-const username = filterArrays.map(obj => obj.username);
+const usernameCall = filterArrays.map(obj => obj.username);
+console.log(usernameCall);
 
 
 //Find out the total score of all users using reduce
+//ANSWER, NOT YET SOLVE
 
-const reduceArrays = array.reduce((score, acc) => {return acc.score + obj.num});
+const Score = []
+const reduceArrays = array.reduce((score, acc, obj, num) => 
+  {
+    return Score.push(obj.score + acc + num)
+  });
+  console.log(Score);
 
 
 
-// (1), what is the value of i?
-// (2), Make this map function pure:
+
+
+// (1), what is the value of i? 
+// (2), Make this map function pure: 
 
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map((num, i) => {
@@ -60,8 +75,50 @@ const newArray = arrayNum.map((num, i) => {
 	return num * 2;
 })
 
-//ANSWER
-//The value of i is 
+//ANSWER(1)
+//The value of i is 1
+
+//ANSWER(2)
+
+const arrayNum = [1, 2, 3, 4, 8, 9];
+const newArray = arrayNum.map((num, i) => {return num * 2});
 
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+
+
+const arrayBonus = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  },
+
+];
+
+const CheckArrays = [];
+
+const BonusArrays = arrayBonus.filter((username, obj, items) => {return obj.items + "!"})
+
+console.log(BonusArrays);
+
+
